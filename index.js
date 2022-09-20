5680b5b31de1cfb9f2f9fe55620bcc3b4a2c0772
unknown <sifatniloy18@gmail.com>
2022-09-20 13:56:53 -0600

    Send data to database using mongodb

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //use middleware
 app.use(cors())
 app.use(express.json());
+
+//user: dbuser1
+//password: z8RhBkCDNnyMweXy
+
+
+
+const uri = "mongodb+srv://dbuser1:z8RhBkCDNnyMweXy@cluster0.ecrrwxp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        const user = { name: 'jhankar', email: 'jhankar@gmail.com' }
+        const result = await userCollection.insertOne(user);
+        console.log(`result inserted with id: ${result.insertedId}`);
+    }
+    finally {
+        // await client.close();
+    }
+}
+run.catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Running my node crud server')
 });
+
+
 
 app.listen(port, () => {
     console.log('crud server is running')
