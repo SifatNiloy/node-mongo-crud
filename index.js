320543ebbe1b28f6f08c64bce7a19515480f0a3c
unknown <sifatniloy18@gmail.com>
2022-09-20 12:46:24 -0600

    project initialized

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+//use middleware
+app.use(cors())
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Running my node crud server')
+});
+
+app.listen(port, () => {
+    console.log('crud server is running')
+})
