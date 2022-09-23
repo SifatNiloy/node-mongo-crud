d5687c796f653baf9facb55a0f147bc381c0d618
unknown <sifatniloy18@gmail.com>
2022-09-22 21:44:22 -0600

    post and get data done

--- index.js
+++ index.js
@@ -20,9 +20,22 @@ async function run() {
     try {
         await client.connect();
         const userCollection = client.db("foodExpress").collection("user");
-        app.post('/user', (req, res) => {
 
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
         })
+
+        //post user : add a new user
+        app.post('/user', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+
+        });
     }
     finally {
         // await client.close();
