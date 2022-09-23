ad8fa4ed98239b832a2d27ab852dc694a601c3fc
unknown <sifatniloy18@gmail.com>
2022-09-23 11:57:58 -0600

    user update in backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +28,12 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         })
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        })
 
         //post user : add a new user
         app.post('/user', async (req, res) => {
@@ -36,6 +43,13 @@ async function run() {
             res.send(result);
 
         });
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
         // await client.close();
